c1d965a5744e92ca39b9cc89ed973b1ef393f582
Hadi <hadi.shayya01@lau.edu>
2024-09-09 21:35:51 +0300

    Solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempreture) {
+  if(tempreture == 32)return 0;
+  return Math.round((tempreture - 32)*(5/9.0)*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempreture) {
+  if(tempreture == 0)return 32;
+  return Math.round((tempreture*(9/5.0) + 32)*10)/10;
 };
 
 // Do not edit below this line
