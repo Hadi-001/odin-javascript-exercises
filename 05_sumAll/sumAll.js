34f73c6a0c8deb5d896ea59b3ebcfb48bcb7392f
Hadi <hadi.shayya01@lau.edu>
2024-09-09 21:12:32 +0300

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+const sumAll = function(first_number,second_number) {
+    if(!Number.isInteger(first_number) || !Number.isInteger(second_number)) return "ERROR";
+    if(first_number > second_number){
+        let temp = first_number;
+        first_number = second_number;
+        second_number = temp;
+    }
+    if(first_number < 0 || second_number < 0)return "ERROR";
+
+    return (second_number*(second_number+1))/2 - (first_number*(first_number-1))/2;
 
 };
 
